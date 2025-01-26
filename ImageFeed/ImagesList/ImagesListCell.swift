65338bdaebe0b7466b0595ed01b57b05dba6e35c
Fortovaya <9777741901@bk.ru>
2025-01-26 19:30:00 +0300

    Создана и кастомизирована ячейка UITableViewCell, установлена связь с UITableView

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,37 @@
+//
+//  ImagesListCellTableViewCell.swift
+//  ImageFeed
+//
+//  Created by Алина on 26.01.2025.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
+    static let reuseIdentifier = "ImagesListCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupCell()
+    }
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+    func setupCell(){
+        cellImage.layer.cornerRadius = 16
+        cellImage.layer.masksToBounds = true
+        
+        dateLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        dateLabel.textColor = UIColor(named: "#FFFFFF")
+    }
+}
