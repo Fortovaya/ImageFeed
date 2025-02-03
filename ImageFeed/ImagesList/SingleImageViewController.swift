6c814e9476fa1b513a74f44b46523656baa22498
Fortovaya <9777741901@bk.ru>
2025-02-03 20:39:56 +0300

    Сверстан экран SingleImageViewController для показа изображения

---
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Алина on 03.02.2025.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    
+}
