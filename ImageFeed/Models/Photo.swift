f1e4d92fb41b5664ed6dd4fd6e8d20d71be51be3
Fortovaya <9777741901@bk.ru>
2025-04-05 23:55:32 +0300

    Реализован класс ProfileLogoutService отвечающий за очистку Cookies из браузера

--- ImageFeed/Models/Photo.swift
+++ ImageFeed/Models/Photo.swift
@@ -15,16 +15,6 @@ struct Photo: Codable {
     let thumbImageURL: URL
     let largeImageURL: URL
     var isLiked: Bool
-    
-//    init(from photoResult: PhotoResult) {
-//        self.id = photoResult.id
-//        self.size = CGSize(width: photoResult.width, height: photoResult.height)
-//        self.createdAt = photoResult.createdAt
-//        self.welcomeDescription = photoResult.description
-//        self.thumbImageURL = photoResult.urls.thumb
-//        self.largeImageURL = photoResult.urls.full
-//        self.isLiked = photoResult.likedByUser
-//    }
 }
 
 extension Photo {
---
+++ ImageFeed/Profile/ProfileLogoutService.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileLogoutService.swift
+//  ImageFeed
+//
+//  Created by Алина on 05.04.2025.
+//
+
+import Foundation
+import WebKit
+
+final class ProfileLogoutService {
+    
+    static let shared = ProfileLogoutService()
+    private init(){}
+    
+    func logout(){
+        cleanCookies()
+    }
+    
+    private func cleanCookies(){
+        HTTPCookieStorage.shared.removeCookies(since: Date.distantPast)
+        WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
+            records.forEach { record in
+                WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler:{})
+                
+            }
+        }
+    }
+}
